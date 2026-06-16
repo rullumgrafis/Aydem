a73a1a3ea10f6d731e14c745eda044266e8a8d01
Fakhrul Ulum <123803648+rullumgrafis@users.noreply.github.com>
2026-06-16 15:53:44 +0700

    5

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoutingController;
 
+Route::get('/', [RoutingController::class, 'index'])->name('root');
 
-Route::get('', [RoutingController::class, 'index'])->name('root');
-Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
-Route::get('{any}', [RoutingController::class, 'root'])->name('any');
+Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])
+    ->where('first', '^(?!images|build).*$');
+
+Route::get('{any}', [RoutingController::class, 'root'])
+    ->where('any', '^(?!images|build).*$');
