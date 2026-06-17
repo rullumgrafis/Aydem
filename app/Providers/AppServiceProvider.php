d7fc24ac76fff85ffb2a5c2ed366d968ddde20b2
Fakhrul Ulum <123803648+rullumgrafis@users.noreply.github.com>
2026-06-17 19:49:07 +0700

    coba (1)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,7 +10,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
-            URL::forceScheme('http');
+            URL::forceScheme('https');
         }
     }
 }
